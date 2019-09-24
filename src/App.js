46f204b8a9cc00d1fe94f55e891294b1b09bce75
lgansel <lgansel@mail.upb.de>
2019-09-24 20:03:11 +0200

    added some more files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from "react";
-
-export default function App() {
-  return <div>Hallo</div>;
+import React, { memo } from 'react'
+import { Board } from './components/screens/Board/Board'
+import './App.css'
+function _App() {
+  return <Board />
 }
+
+export const App = memo(_App)
---
+++ src/components/screens/Board/Board.js
@@ -0,0 +1,7 @@
+import React, { memo } from 'react'
+
+function _Board() {
+  return <div></div>
+}
+
+export const Board = memo(_Board)
