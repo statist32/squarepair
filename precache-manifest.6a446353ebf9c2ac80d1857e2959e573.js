20b22f3dd77f884973241191432bf964012288f7
lgansel <lgansel@mail.upb.de>
2019-09-28 22:13:23 +0200

    Updates

---
+++ precache-manifest.6a446353ebf9c2ac80d1857e2959e573.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d99102b0d654b21661d90bdca1372b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc3ba303648e74515d61",
+    "url": "/static/css/main.a4edfe54.chunk.css"
+  },
+  {
+    "revision": "fa0bb54df34a590c0d28",
+    "url": "/static/js/2.edcff3d9.chunk.js"
+  },
+  {
+    "revision": "fc3ba303648e74515d61",
+    "url": "/static/js/main.ef82d5fc.chunk.js"
+  },
+  {
+    "revision": "3f3e7dcb3148640208fc",
+    "url": "/static/js/runtime-main.5c9871ee.js"
+  }
+]);
