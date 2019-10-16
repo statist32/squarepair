b112aa535c7d4988181866fc5beed694b75efe0f
lgansel <lgansel@mail.upb.de>
2019-10-16 21:01:58 +0200

    added iterative mode

--- gameSolver.cpp
+++ gameSolver.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <cmath>
 using namespace std;
 
 /* helper function to display solutions */
@@ -89,76 +90,57 @@ void findSolutionRecursive(int rows, int columns, vector<vector<vector<int>>> hi
         }
     }
 }
-void testEachTile(int rows, int columns, vector<vector<vector<int>>> history, int clickAmount, int colorAmount)
+
+vector<int> incrementClicks(vector<int> clicks, int rows, int columns)
 {
-    vector<vector<int>> board(history.back());
-    for (int row = 0; row < rows; row++)
+    vector<int> tempClicks(clicks);
+    const int dimension = rows * columns;
+    const int size = tempClicks.size();
+    bool finished = false;
+    for (int i = size - 1; i >= 0 && !finished; i--)
     {
-        for (int column = 0; column < columns; column++)
+        tempClicks[i] = (tempClicks[i] + 1) % dimension;
+        if (tempClicks[i])
         {
-            vector<vector<int>> tempBoard = clickBoard(row, column, board, colorAmount);
-            vector<vector<vector<int>>> tempHistory(history);
-            tempHistory.push_back(tempBoard);
-            if (checkWin(tempBoard))
+            finished = true;
+        }
+        if (!i && !finished)
+        {
+            tempClicks.push_back(0);
+        }
+    }
+
+    return tempClicks;
+}
+
+void findSolutionIterative(int rows, int columns, vector<vector<int>> startBoard, int clickAmount, int colorAmount)
+{
+    vector<vector<int>> board(startBoard);
+    vector<int> clicks(1, 0);
+    bool finished = false;
+    for (int rounds = 0; (rounds < pow((rows * columns), 10)) && !finished; rounds++)
+    {
+        vector<vector<int>> tempBoard(board);
+        for (int click : clicks)
+        {
+            int row = click / columns;
+            int column = click % columns;
+
+            tempBoard = clickBoard(row, column, tempBoard, colorAmount);
+        }
+        if (checkWin(tempBoard))
+        {
+            cout << "Solved in " << clicks.size() << " steps with these steps" << endl;
+            for (int click : clicks)
             {
-                printHistory(tempHistory);
+                cout << click << " ";
             }
+            cout << endl;
+            finished = true;
         }
+
+        clicks = incrementClicks(clicks, rows, columns);
     }
-}
-
-vector<vector<int>> manageCounter(vector<vector<int>> tempCounter, int colorAmount)
-{
-    vector<vector<int>> counter(tempCounter);
-    for (int row = 0; row < counter.size(); row++)
-    {
-        for (int column = 0; column < counter[0].size(); column++)
-        {
-            while (counter[row][column] >= colorAmount)
-            {
-                counter[row][column] -= colorAmount;
-                if (column + 1 < counter[0].size())
-                {
-                    counter[row][column + 1]++;
-                }
-                else if (row + 1 < counter.size())
-                {
-                    counter[row + 1][0]++;
-                }
-            }
-        }
-    }
-    return counter;
-}
-
-vector<vector<int>> increaseCounter(vector<vector<int>> tempCounter, int colorAmount)
-{
-    vector<vector<int>> counter(tempCounter);
-    counter[0][0]++;
-    counter = manageCounter(counter, colorAmount);
-    return counter;
-}
-
-vector<vector<int>> addBoards(vector<vector<int>> board1, vector<vector<int>> board2, int colorAmount)
-{
-    vector<vector<int>> tempBoard(board1);
-    for (int row = 0; row < tempBoard.size(); row++)
-    {
-        for (int column = 0; column < tempBoard[0].size(); column++)
-        {
-            tempBoard[row][column] += board2[row][column];
-        }
-    }
-    tempBoard = manageCounter(tempBoard, colorAmount);
-    return tempBoard;
-}
-
-void findSolution(int rows, int columns, vector<vector<vector<int>>> history, int clickAmount, int colorAmount)
-{
-    vector<vector<int>> board(history.back());
-    vector<vector<int>> counter(rows, {0, 1, 2});
-    counter = addBoards(board, counter, colorAmount);
-    printBoard(counter);
 }
 
 int main(int argc, char *argv[])
@@ -167,10 +149,13 @@ int main(int argc, char *argv[])
     int columns = 3;
     int colorAmount = 3;
     int clickAmount = 1;
-    vector<vector<int>> board = {{1, 1, 1}, {1, 1, 0}, {1, 0, 0}};
-    vector<vector<vector<int>>> history(1, board);
-
-    findSolution(rows, columns, history, clickAmount, colorAmount);
+    vector<vector<int>> board = {{1, 1, 1},
+                                 {1, 1, 1},
+                                 {1, 1, 0}};
+    const auto start = chrono::steady_clock::now();
+    findSolutionIterative(rows, columns, board, clickAmount, colorAmount);
+    const auto end = chrono::steady_clock::now();
+    cout << "Done in " << chrono::duration_cast<chrono::seconds>(end - start).count() << " seconds!" << endl;
 }
 
 // for (int click = 1; click < clickAmount; click++)
